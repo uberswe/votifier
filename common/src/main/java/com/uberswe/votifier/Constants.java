cc6b1a0563e3eaec1ec18536cd9f3a96462941e0
uberswe <admin@uberswe.com>
2026-03-14 03:08:47 +0100

    Rename to Modded Votifier and update JAR filenames
    Change display name from Votifier to Modded Votifier in gradle
    properties, Constants.java, release workflow, and README. Change
    archive base names to modded-votifier-<loader> in all build files.

--- common/src/main/java/com/uberswe/votifier/Constants.java
+++ common/src/main/java/com/uberswe/votifier/Constants.java
@@ -6,6 +6,6 @@ import org.slf4j.LoggerFactory;
 public class Constants {
 
     public static final String MOD_ID = "votifier";
-    public static final String MOD_NAME = "Votifier";
+    public static final String MOD_NAME = "Modded Votifier";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
